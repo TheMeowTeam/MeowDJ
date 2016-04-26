9a6abb4e92f3a0f60da9761cc9a14a933545a586
Thog <thog92@hotmail.fr>
2016-04-27 01:16:49 +0200

    Add Login button/Create - Join room on the index page

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -8,20 +8,35 @@
 module.exports = {
 
 
-
   /**
    * `HomeController.index()`
    *
    * Fired when a user wants to load the home page
    */
   index: function (req, res) {
+    data = {
+      title: 'Home',
+      page: 'home',
+      user: req.session.user
+    };
+    if (req.session.user) {
+      Room.findOne({
+        owner: req.session.user.id
+      }, function (err, room) {
 
-    return res.view('home', {
-      data: {
-        title: 'Home',
-        page: 'home'
-      }
-    });
+        if (err) {
+          return res.serverError(err);
+        }
+
+        if (room) {
+          data['userRoom'] = room.identifier;
+        }
+
+        return res.view('home', {data : data});
+      });
+    }
+    else
+      return res.view('home', {data : data});
   }
 };
 
